0fc1161aad81b56d622dbd18f4db368503a31b6b
Levi Mardis <levimardis@gmail.com>
2021-07-03 16:37:31 -0500

    Button changes

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.getElementById("startStop").addEventListener("click", start);
+document.getElementById("reset").addEventListener("click", reset);
+
+document.getElementById("timer").innerHTML = 0.0
--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,5 @@
 const timer = document.getElementById("timer");
+const button = document.getElementById("reset")
 let interval;
 let pause = false;
 let time = 0
@@ -7,10 +8,12 @@ const start = () => {
   if (!pause) {
     interval = setInterval(update, 100);
     pause = true;
+    button.disabled = true
   } else {
     clearInterval(interval);
     interval = null;
     pause = false;
+    button.disabled = false
   }
 };
 const update = () => {
@@ -18,13 +21,13 @@ const update = () => {
   time += 0.1;
 };
 const reset = () => {
+  if (pause) {
+    return
+  }
   clearInterval(interval);
   interval = null;
   pause = false;
+  button.disabled = true
   time = 0
   timer.innerHTML = time
 };
-
-timer.innerHTML = time;
-document.getElementById("startStop").addEventListener("click", start);
-document.getElementById("reset").addEventListener("click", reset);
