92df09788613b883257b1833aad6ebe64d58b62b
Levi Mardis <levimardis@gmail.com>
2021-07-03 12:24:35 -0500

    Stopwatch logic done

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-let timer = 10
-let pause = true
+const timer = document.getElementById("timer");
+let interval;
+let pause = false;
+let userTime = 3;
+let time = userTime
 
-
-const count = () => {
-    display.innerHTML = timer
-    if (timer <= 0 || pause === true) {
-        clearInterval(time)
-    }
-    timer --
-    if (pause) {
-        clearInterval(time)
-    }
-}
 const start = () => {
-    if (pause) {
-        pause = false
-        let time = setInterval(() => {
-            display.innerHTML = timer
-            if (timer <= 0 || pause === true) {
-                clearInterval(time)
-            }
-            timer --
-        }, 1000)
-    } else {
-        pause = true
-    }
-}
+  if (!pause) {
+    interval = setInterval(update, 100);
+    pause = true;
+  } else {
+    clearInterval(interval);
+    interval = null;
+    pause = false;
+  }
+};
+const update = () => {
+  if (time < 0) {
+    reset('DONE')
+    setTimeout(() => {timer.innerHTML = userTime}, 2000)
+    return
+  }
+  timer.innerHTML = time.toFixed(1);
+  time -= 0.1;
+};
+const reset = (display) => {
+  clearInterval(interval);
+  interval = null;
+  pause = false;
+  time = userTime;
+  timer.innerHTML = display
+};
 
-
-
-const display = document.getElementById('timer')
-document.getElementById('startButton').addEventListener('click', start)
-/* changes */
+timer.innerHTML = time;
+document.getElementById("startStop").addEventListener("click", start);
+document.getElementById("reset").addEventListener("click", () => {reset(userTime)});
