ad56b224a81573661a8b586e7b4826ce50eca808
Levi Mardis <levimardis@gmail.com>
2021-07-03 15:32:35 -0500

    Final stopwatch

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const timer = document.getElementById("timer");
 let interval;
 let pause = false;
-let userTime = 3;
-let time = userTime
+let time = 0
 
 const start = () => {
   if (!pause) {
@@ -15,22 +14,17 @@ const start = () => {
   }
 };
 const update = () => {
-  if (time < 0) {
-    reset('DONE')
-    setTimeout(() => {timer.innerHTML = userTime}, 2000)
-    return
-  }
   timer.innerHTML = time.toFixed(1);
-  time -= 0.1;
+  time += 0.1;
 };
-const reset = (display) => {
+const reset = () => {
   clearInterval(interval);
   interval = null;
   pause = false;
-  time = userTime;
-  timer.innerHTML = display
+  time = 0
+  timer.innerHTML = time
 };
 
 timer.innerHTML = time;
 document.getElementById("startStop").addEventListener("click", start);
-document.getElementById("reset").addEventListener("click", () => {reset(userTime)});
+document.getElementById("reset").addEventListener("click", reset);
