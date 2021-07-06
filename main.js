8f6ad58568ab6266b4c70a41a71520e1251a7961
Levi Mardis <levimardis@gmail.com>
2021-07-05 20:59:31 -0500

    Latest changes

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const coffeeSlider = new Slider("coffee-slider", "coffee");
+const coffeeSlider = new Slider("coffee-slider", "coffee", 20, "");
+const ratioSlider = new Slider("ratio-slider", "ratio", 40, "1:");
+const waterSlider = new Slider("water-slider", "ratio", 20, "");
 coffeeSlider.initialize();
+ratioSlider.initialize();
+waterSlider.initialize();
 
 document.getElementById("startStop").addEventListener("touchstart", start);
 document.getElementById("reset").addEventListener("touchstart", reset);
--- slider.js
+++ slider.js
@@ -1,34 +1,57 @@
 class Slider {
-  constructor(container, display) {
+  constructor(container, display, speed, format, base) {
     this.divId = document.getElementById(container);
+    this.preText = document.getElementById("pre-" + display);
+    this.postText = document.getElementById("post-" + display);
     this.textDisplay = document.getElementById(display);
     this.xd = null;
     this.x = 0;
     this.output = 0;
+    this.speed = speed;
+    this.format = format;
+    this.base = base;
   }
 
   startSlide = (e) => {
+    // Save screen touch x coordinates and begin listening for touch move
     this.xd = e.touches[0].clientX;
-    //this.divId.addEventListener("mousemove", this.numChange);
     this.divId.addEventListener("touchmove", this.numChange);
   };
 
   numChange = (e) => {
-    this.x = (e.touches[0].clientX - this.xd) / 20;
-    this.textDisplay.innerHTML = (this.x + this.output).toFixed(0);
+    // Set the incrememnt speed based on movement
+    this.x = (e.touches[0].clientX - this.xd) / this.speed;
+    let textOut = (this.x + this.output) * -1;
+    // If 0 or negative hide pre text and keep current display set to 0
+    if (Math.floor(textOut) <= 0) {
+      this.preText.innerHTML = `${this.format}${this.base}`
+      this.postText.innerHTML = `${ this.format } ${ this.base + 1 }`;
+      this.textDisplay.innerHTML = `0`;
+      this.output = 0;
+    } else {
+    // Display pre/post/current numbers
+      this.preText.innerHTML = `${this.format}${Math.floor(textOut - 1)}`;
+      this.postText.innerHTML = `${this.format}${Math.floor(textOut + 1)}`;
+      this.textDisplay.innerHTML = `${this.format}${Math.floor(textOut)}`;
+    }
   };
 
   initialize = () => {
-    //this.divId.addEventListener("mousedown", this.startSlide);
+    // Begin listening for touch start and end events
     this.divId.addEventListener("touchstart", this.startSlide);
-
-    /*document.addEventListener("mouseup", () => {
-      this.output += this.x;
-      this.divId.removeEventListener("mousemove", this.numChange);
-    });*/
     document.addEventListener("touchend", () => {
-      this.output += this.x;
+      // If there has been a corrext touch in this element log the touch end results, else do nothing
+      if (this.xd !== null) {
+        this.output += this.x;
+        this.xd = null;
+      }
       this.divId.removeEventListener("touchmove", this.numChange);
     });
   };
+  get out() {
+    return this.output
+  }
+  set out(x) {
+    this.output 
+  }
 }
